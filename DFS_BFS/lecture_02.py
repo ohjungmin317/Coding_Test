1b3e02ba034e9c25a6fbb16d07ad15936bbbd11d
오정민 <90691262+ohjungmin317@users.noreply.github.com>
2022-07-15 22:29:57 +0900

    22_07_15
    미로찾기 p152 - BFS문제

---
+++ DFS_BFS/lecture_02.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 14 20:10:15 2022
+
+@author: 오정민
+"""
+from collections import deque
+
+n,m = map(int, input().split())
+
+graph=[]
+
+for i in range(n):
+    graph.append(list(map(int,input())))
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs(x,y):
+    queue = deque()
+    queue.append((x,y))
+    
+    while queue:
+        x,y = queue.popleft()
+        
+        for i in range(4):
+            
+            nx = x + dx[i]
+            ny = y + dy[i]
+            
+            if not(0 <= nx < n and 0 <= ny <m):
+                continue
+            
+            if graph[nx][ny] == 0:
+                continue
+            
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx,ny))
+                
+    return graph[n-1][m-1]
+
+print(bfs(0,0))                
+        
+
+
