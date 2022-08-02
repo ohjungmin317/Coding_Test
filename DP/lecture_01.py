a991f7e7edff5ff5bc76bb091f274b5aca22b869
오정민 <90691262+ohjungmin317@users.noreply.github.com>
2022-08-02 19:37:32 +0900

    22_08_02
    개미전사 점화식 : d[i] = max(d[i-1], d[i-2]+storage[i])

---
+++ DP/lecture_01.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  2 17:06:53 2022
+
+@author: 오정민
+"""
+
+# p220 개미전사
+
+# 문제 접근 : 
+    # (1) 식량창고는 일직선으로 연결이 되어 있다.
+    # (2) 서로 인접한 창고는 공격을 할 수 없고 한 칸 이상 떨어진 식량 창고만 공격이 가능하다.
+    # (3) 얻을 수 있는 식량의 최댓값을 구해라
+
+
+n = int(input())
+storage = list(map(int, input().split()))
+
+d = [0] * 100 
+
+d[0] = storage[0]
+d[1] = max(storage[0], storage[1])
+
+for i in range(2,n):
+    print('f(' + str(storage) +')', end =' ')
+    d[i] = max(d[i-1], d[i-2]+storage[i])
+
+
+print(d[n-1])
+    
