d986d5b08a6c1fa605e5a35a75f87e3df960e81d
오정민 <90691262+ohjungmin317@users.noreply.github.com>
2022-08-08 20:46:29 +0900

    22_08_08
    Floyd algorithm for python

---
+++ shortest_path/floyd.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  8 18:30:33 2022
+
+@author: 오정민
+"""
+
+INF = int(1e9)
+
+# 노드의 개수 및 간선의 개수를 입력 받기
+
+n = int(input())
+m = int(input())
+
+# 2차원 리스트(그래프 표현)를 만들고, 무한으로 초기화 
+graph = [[INF]*(n+1) for _ in range(n+1)] # 행과 열이 구성이 될 수 있또록 2차원 배열을 만든다.
+
+# 자기 자신에게 자기신으로 돌아가는 비용은 0으로 초기화 
+for a in range(1, n+1):
+    for b in range(1, n+1):
+        if a == b:
+            graph[a][b] = 0
+
+# 각 간선에 대한 정보를 입력받아 그 값으로 초기화
+for _ in range(m):
+    # A에서 B로 가는 비용을 C라고 설정 
+    a,b,c = map(int, input().split())
+    graph[a][b] = c
+    
+# 점화식에 따라 플로이드 워셜 알고리즘 수행
+for k in range(1, n+1):
+    for a in range(1, n+1):
+        for b in range(1, n+1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+
+# 수행된 결과를 출력
+for a in range(1, n+1):
+    for b in range(1, n+1):
+        # 도달 할 수 없는 경우 무한(inf)출력
+        if graph[a][b] == INF:
+            print("INFINITY", end=" ")
+            
+        else:
+            print(graph[a][b], end= " ")
+print()
